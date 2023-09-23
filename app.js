60c95b8944de73f2f913272056cbfe4d71b9d667
Jordan G. Marcelo <129609081+JDMarcelo@users.noreply.github.com>
2023-09-24 01:48:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //check if the SW is supported
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then((reg) => {
+    navigator.serviceWorker.register('sw.js').then((reg) => {
       console.log('Service worker registered');
       // Access the `reg` variable here
     }).catch((err) => {
